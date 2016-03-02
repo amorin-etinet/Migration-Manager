4ac90a3ac824b5db83835459b8f3d1086cd650f1
amorin-etinet <noreply>
2016-03-02 21:42:09 +0000

    Add module configuration

---
+++ ui/configuration.js
@@ -0,0 +1,15 @@
+/**
+ * @module ui/configuration
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Configuration
+ * @extends Montage
+ */
+exports.Configuration = Montage.specialize(/** @lends Configuration# */ {
+    constructor: {
+        value: function Configuration() {
+            this.super();
+        }
+    }
+});
